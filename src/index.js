8c3860de20c09775768ae565bcd3d48206a9d58b
Nathan Cai <wenjuncai97@gmail.com>
2022-04-20 00:54:13 -0400

    added logger

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
+import logger from 'redux-logger';
 import reducer from './reducers';
 
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
+
 
 ReactDOM.render(
   <Provider store={store}>
